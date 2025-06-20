7db75b5cca942875dad9951df37f0ab463f46b85
Peter Chege <emchegep@gmail.com>
2025-06-20 15:16:35 +0300

    added job and command test

---
+++ app/Console/Commands/test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\ProcessPodcast;
+use Illuminate\Console\Command;
+
+class test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dispatch:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        ProcessPodcast::dispatch();
+    }
+}
